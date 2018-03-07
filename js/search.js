190d0a1be57a082519c4290a9371872cd819672f
megazzz <36747287+megazzz@users.noreply.github.com>
2018-03-07 15:37:56 -0700

    4000

--- js/search.js
+++ js/search.js
@@ -1,16 +1,11 @@
-// remap jQuery to $
-(function($){
-
-	/* trigger when page is ready */
-	$(document).ready(function (){
-	
-		// your functions go here
-		$('#email-field').click(function() {
-			$(this).addClass("active");
-       $(this).attr('placeholder','Email Address...');
-			$('#subscribe-button').addClass("show");
-		});
-	
-	});
-
+(function($) {
+  /* trigger when page is ready */
+  $(document).ready(function() {
+    // your functions go here
+    $("#email-field").click(function() {
+      $(this).addClass("active");
+      $(this).attr("placeholder", "Email Address...");
+      $("#subscribe-button").addClass("show");s
+    });
+  });
 })(window.jQuery);
